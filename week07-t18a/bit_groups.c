6dc11218e2975d0f443beda1276c424c204028ad
abiramen <7523422+abiramen@users.noreply.github.com>
2022-07-12 20:38:29 +1000

    week07-t18a: add code

---
+++ week07-t18a/bit_groups.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+ 
+typedef struct six_bit_groups {
+    uint8_t middle_bits;
+    uint8_t lower_bits;
+} six_bit_groups_t;
+ 
+/* 
+For example:
+00000000 00000101 01000000 01110110 
+
+ 
+middle six bits are 101010
+ lower six bits are 110110  
+*/
+ 
+// TODO
+six_bit_groups_t get_bit_groups(uint32_t value) {
+    six_bit_groups_t result = {
+        .middle_bits = (value << 13) & 0x3f,
+        .lower_bits = value & 0x3f
+    };
+    return result;
+}
+ 
+int main(void) {
+    return 0;
+}
