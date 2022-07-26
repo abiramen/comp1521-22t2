48b20f924e8454535e68851981da178baaf5e039
abiramen <7523422+abiramen@users.noreply.github.com>
2022-07-26 19:38:45 +1000

    week09-t18a: add code

---
+++ week08-w16a/first_line.c
@@ -0,0 +1,39 @@
+// Write a C program, first_line.c, which is given one command-line argument -
+// the name of a file - and which prints the first line of that file to stdout.
+// If given an incorrect number of arguments, or if there was an error opening
+// the file, it should print a suitable error message.
+//
+// A line may be of any size.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+    // TODO: complete this function
+
+    if (argc != 2) {
+        printf("usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    FILE *stream = fopen(argv[1], "r");
+    if (stream == NULL) {
+        perror(argv[1]);
+        return 1;
+    }
+
+    int c;
+
+    while ((c = fgetc(stream)) != EOF && c != '\n') {
+        putchar(c);
+    }
+    putchar('\n');
+
+    fclose(stream);
+
+    return 0;
+}
